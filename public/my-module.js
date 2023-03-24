face880904b42f0dfc254356eee6d6a1c91da515
gazzen <gazensuwal@gmail.com>
2023-03-24 10:50:12 -0400

    my commit

---
+++ public/my-module.js
@@ -0,0 +1,13 @@
+import { theShire, buckland } from "./another-module.js";
+
+
+
+
+
+
+var statement = () =>
+  `${theShire.name} was founded ${
+    buckland.founding - theShire.founding
+  } years before ${buckland.name}.`;
+
+export { theShire, statement };
