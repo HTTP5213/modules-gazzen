4d807b7461d2ba205aa2f4d300ec87d90bc930f9
gazzen <gazzen@users.noreply.github.com>
2023-04-02 22:21:38 -0400

    changes

--- test/my-module.test.js
+++ test/my-module.test.js
@@ -1,17 +1,15 @@
 import { HobbitSettlement } from '../public/another-module.js';
-
 import { theShire, statement } from '../public/my-module.js';
-
 import { expect } from 'chai';
 
 describe('The Shire', function() {
-    it('should be Hobbit Settlement', function() {
-        expect(theShire).to.be.an.instanceof(HobbitSettlement);
-    });
+  it('should be Hobbit Settlement', function() {
+    expect(theShire).to.be.an.instanceof(HobbitSettlement);
+  });
 });
 
 describe('statement', function() {
-	it('Should return the correct string', () => {
-		expect(statement()).to.equal('The Shire was founded 739 years before Buckland.');
-	});
+  it('Should return the correct string', () => {
+    expect(statement()).to.equal('The Shire was founded 739 years before Buckland.');
+  });
 });
