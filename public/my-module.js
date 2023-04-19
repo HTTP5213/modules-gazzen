6a671c839cbb0ef05b857ba10ab3b86895ec6ba4
gazzen <gazensuwal@gmail.com>
2023-04-19 18:33:42 -0400

    updates

--- public/my-module.js
+++ public/my-module.js
@@ -1,6 +1,6 @@
-import { theShire, buckland } from "./another-module.js";
-var statement = () =>
+import { theShire, buckland } from './another-module.js';
+let statement = () =>
   `${theShire.name} was founded ${
     buckland.founding - theShire.founding
-  } years before ${buckland.name}.`;
-export { theShire, statement };
+  } years before ${buckland.name} `;
+export { theShire , statement };
