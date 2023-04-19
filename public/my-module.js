f80e127962cce1273e6a7e92a46694ee8b899704
gazzen <gazensuwal@gmail.com>
2023-04-19 18:54:36 -0400

    changes

--- public/my-module.js
+++ public/my-module.js
@@ -1,5 +1,5 @@
 import { theShire, buckland } from './another-module.js';
-let statement = () =>
+const statement = () =>
   `${theShire.name} was founded ${
     buckland.founding - theShire.founding
   } years before ${buckland.name} `;
